637179722dba6231d4cf4f8671e9c3ee1cacc492
Nikoloz Kekelidze <107684449+NikolozK34@users.noreply.github.com>
2023-12-20 14:18:26 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class davaleba4_2_1
+{
+    static void Main()
+    {
+        int[] first = { 10, 20, 30 };
+
+        int[] second = new int[first.Length];
+
+        for (int i = 0; i < first.Length; i++)
+        {
+            second[i] = first[i];
+        }
+
+        Console.WriteLine("Second massive:");
+        foreach (int number in second)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
